228d68d22a242c3227a337730a934a153b1d5439
FelixPriori <felixriouxsabourin@gmail.com>
2020-01-08 19:14:00 +0000

    Refactored the code for loopy lighthouse and added functionalities

---
+++ arrays/loopy-lighthouse-two.js
@@ -0,0 +1,20 @@
+const loopyLighthouse = function(range, multiples, words){
+
+  const loopy = words[0];
+  const lighthouse = words[1];
+
+  for (let i = range[0]; i <= range[1]; i++){
+    if (i % multiples[0] === 0 && i % multiples[1] === 0){
+      console.log(loopy + lighthouse);
+    } else if (i % multiples[0] === 0){
+      console.log(loopy);
+    } else if (i % multiples[1] === 0){
+      console.log(lighthouse);
+    } else {
+      console.log(i);
+    }
+  }
+
+}
+
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
